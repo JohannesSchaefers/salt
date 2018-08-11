9f335761b0746a154fe165ad2901959794b20875
Johannes Schäfers <info@iconicity.de>
2018-08-11 20:54:07 +0200

    Rainer

--- IdeaProjects/zucker/src/sample/Main.java
+++ IdeaProjects/zucker/src/sample/Main.java
@@ -11,7 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World!!!!!!ooo");
+        primaryStage.setTitle("Hello Rainer");
+
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
