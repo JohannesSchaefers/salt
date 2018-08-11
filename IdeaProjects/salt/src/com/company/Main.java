57ab35c00f752415884e887f2321f4b72fe7af89
Johannes Schäfers <info@iconicity.de>
2018-08-11 22:07:49 +0200

    sout hinzugefügt sout hinzugefügt

--- IdeaProjects/salt/src/com/company/Main.java
+++ IdeaProjects/salt/src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("3434");
+        System.out.print("3434111111111111");
+        System.out.print("3434111111111111");
 	// write your code here
     }
 }
